9209f02bd1396acd4d684dae35af2f62fd1f2355
Heidi Fryzell <heidi37@users.noreply.github.com>
2025-12-15 08:18:05 -0500

    [TASK] Create job and card-related components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import HomeCards from "./components/HomeCards"
+import JobListings from "./components/JobListings"
+
 
 const App = () => {
-  const name = "Helvetica"
   return (
     <>
-      <div className="font-sans text-5xl">This should be {name}</div>
-      <div className="grid grid-cols-[var(--grid-70-30)] gap-4">
-        <div className="bg-blue-500 text-white">70%</div>
-        <div className="bg-red-500 text-white">30%</div>
-      </div>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+
+      
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
     </>
   )
 }
