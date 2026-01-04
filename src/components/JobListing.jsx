e97e6815ca6ffbe0ae0e131c368031baea2e1d72
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-01-04 14:08:05 -0500

    [TASK] Add dynamic Read More or Less option

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,7 +1,18 @@
 import React from "react"
+import { useState } from "react"
+import { FaMapMarker } from "react-icons/fa"
 
 const JobListing = ( { job } ) => {
   const { id, title, type, description, salary, location } = job
+  
+  const [showFullDescription, setShowFullDescription] = useState(false)
+
+  let jobDescription = description
+
+  if(!showFullDescription) {
+    jobDescription = description.substring(0, 90) + '...'
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -10,7 +21,9 @@ const JobListing = ( { job } ) => {
           <h3 className="text-xl font-bold">{title}</h3>
         </div>
 
-        <div className="mb-5">{description}</div>
+        <div className="mb-5">{jobDescription}</div>
+
+        <button onClick={() => setShowFullDescription(prev => !prev)} className="text-indigo-500 mb-5 hover:text-indigo-600">{ showFullDescription ? "Less" : "More" }</button>
 
         <h3 className="text-indigo-500 mb-2">{salary} / Year</h3>
 
@@ -18,7 +31,7 @@ const JobListing = ( { job } ) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline ext-lg mb-1 mr-1" />
             {location}
           </div>
           <a
