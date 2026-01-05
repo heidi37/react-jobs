d6be68af98dd49e55d8e5adf33bb4d5be71ebd01
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-01-05 08:53:59 -0500

    [TASK] Set up custom 404 page

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useState } from "react"
 import { FaMapMarker } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 const JobListing = ( { job } ) => {
   const { id, title, type, description, salary, location } = job
@@ -34,12 +35,12 @@ const JobListing = ( { job } ) => {
             <FaMapMarker className="inline ext-lg mb-1 mr-1" />
             {location}
           </div>
-          <a
-            href={`/job/${id}`}
+          <Link
+            to={`/job/${id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
