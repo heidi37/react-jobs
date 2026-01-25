b63f219d5da15475bbf9f35e3bbbf7c43f5e5f2c
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-01-25 14:42:33 -0500

    [TASK] Implement Job updating

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,9 +1,22 @@
 import React from "react"
-import { useLoaderData, Link } from "react-router-dom"
+import { useLoaderData, Link, useNavigate } from "react-router-dom"
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa"
+import { toast } from 'react-toastify'
 
-const JobPage = () => {
+const JobPage = ( {deleteJob} ) => {
+  const navigate = useNavigate()
   const job = useLoaderData()
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm('Are you sure you want to delete that job?')
+
+    if (!confirm) {
+      return
+    } else {
+      deleteJob(jobId)
+      toast.success('Job deleted sucesssfully')
+    }
+    navigate('/jobs')
+  }
 
   return (
     <>
@@ -72,12 +85,12 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={ `/jobs/edit/${job.id}` }
+                  to={ `/edit-job/${job.id}` }
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button onClick={ () => onDeleteClick(job.id) } className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Delete Job
                 </button>
               </div>
@@ -90,7 +103,6 @@ const JobPage = () => {
 }
 
 const jobLoader = async ({ params }) => {
-  console.log(params)
   const res = await fetch(`/api/jobs/${params.id}`)
   const data = await res.json()
   return data
